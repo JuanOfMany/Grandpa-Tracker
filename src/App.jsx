5ca3e9207e6cd29a9de1a1a200f2a886eca56f27
Juan Pinol <JuannnCodes@gmail.com>
2023-08-28 22:48:45 -0400

    Implemented an example chart using chart.js

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import Graph from './components/Graph.jsx';
+
+export default function App () {
+
+  return (
+    <div id="app-container">
+      <div>I should add a banner or something.</div>
+      <span>Here's some text...</span>
+      <Graph />
+      <div>Some other stuff.</div>
+    </div>
+  )
+}
--- src/index.js
+++ src/index.js
@@ -1,7 +1,6 @@
 import { createRoot } from 'react-dom/client';
+import App from './App.jsx';
+import "./index.css"
 
-document.body.innerHTML = '<div id="app"></div>';
-
-// Render your React component instead
 const root = createRoot(document.getElementById('app'));
-root.render(<h1>Hello, world</h1>);
+root.render(<App />);
