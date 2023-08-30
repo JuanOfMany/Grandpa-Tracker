c1193528f293426dc6bfff5828b336a6f01d1c20
Juan Pinol <JuannnCodes@gmail.com>
2023-08-30 14:34:41 -0400

    Changed to a scatter plot from line chart.

--- src/components/Graph.jsx
+++ src/components/Graph.jsx
@@ -1,26 +1,41 @@
 import React from 'react';
-import Chart from "chart.js/auto";
-import { Line } from "react-chartjs-2";
+import {
+  Chart as ChartJS,
+  LinearScale,
+  PointElement,
+  LineElement,
+  Tooltip,
+  Legend,
+} from 'chart.js';
+import { Scatter } from 'react-chartjs-2';
 
 export default function Graph () {
 
-  const labels = ["January", "February", "March", "April", "May", "June"];
+  ChartJS.register(LinearScale, PointElement, LineElement, Tooltip, Legend);
+
+  const options = {
+    scales: {
+    },
+  };
 
   const data = {
-    labels: labels,
     datasets: [
       {
-        label: "My First dataset",
-        backgroundColor: "rgb(255, 99, 132)",
-        borderColor: "rgb(255, 99, 132)",
-        data: [0, 10, 5, 2, 20, 30, 45],
+        label: "Grandpa's Blood Pressure",
+        data: [
+          {x:10, y:14},
+          {x:25, y:35},
+          {x:21, y:20},
+          {x:35, y:28},
+          {x:15, y:10},
+          {x:19, y:30},
+        ],
+        backgroundColor: 'rgba(255, 99, 132, 1)',
       },
     ],
   };
 
   return (
-    <div id="chart-holder" >
-      <Line data={data} />
-    </div>
+      <Scatter id="chart" options={options} data={data} />
   )
 }
