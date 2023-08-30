bd4b84746eb9be5618fe39a63023ecb54c3fe465
Juan Pinol <JuannnCodes@gmail.com>
2023-08-30 16:48:41 -0400

    Added multi X axis labels to prepare for day/month/year display

--- src/components/Graph.jsx
+++ src/components/Graph.jsx
@@ -1,20 +1,54 @@
-import React from 'react';
+import React from "react";
 import {
   Chart as ChartJS,
   LinearScale,
+  TimeScale,
+  LogarithmicScale,
   PointElement,
   LineElement,
   Tooltip,
   Legend,
-} from 'chart.js';
-import { Scatter } from 'react-chartjs-2';
+} from "chart.js";
+import { Scatter } from "react-chartjs-2";
+import { enUS } from "date-fns/locale";
+import format from "date-fns/format";
+import 'chartjs-adapter-date-fns';
 
-export default function Graph () {
+export default function Graph() {
+  const exampleDate = Date.now();
+  console.log(exampleDate);
 
-  ChartJS.register(LinearScale, PointElement, LineElement, Tooltip, Legend);
+  ChartJS.register(LinearScale, TimeScale, LogarithmicScale, PointElement, LineElement, Tooltip, Legend);
 
   const options = {
+    showLine: false,
     scales: {
+      y: {
+        min: 60,
+        max: 180,
+      },
+      x:
+        {
+          ticks: {
+            callback: function (value, index, ticks) {
+              return format(value, "d MMM");
+            },
+          },
+          type: 'time',
+          adapters: {
+            date: {
+              locale: enUS,
+            }
+          }
+        },
+        months: {
+          type: 'linear',
+          position: 'bottom',
+        },
+        years: {
+          type: 'linear',
+          position: 'bottom',
+        }
     },
   };
 
@@ -23,19 +57,17 @@ export default function Graph () {
       {
         label: "Grandpa's Blood Pressure",
         data: [
-          {x:10, y:14},
-          {x:25, y:35},
-          {x:21, y:20},
-          {x:35, y:28},
-          {x:15, y:10},
-          {x:19, y:30},
+          { x: Date.now(), y: 120 },
+          { x: 1694428361748, y: 130 },
+          { x: 1696428366748, y: 110 },
+          { x: 1691428461748, y: 100 },
+          { x: 1699425361748, y: 150 },
+          { x: 1697528361748, y: 170 },
         ],
-        backgroundColor: 'rgba(255, 99, 132, 1)',
+        backgroundColor: "rgba(255, 99, 132, 1)",
       },
     ],
   };
 
-  return (
-      <Scatter id="chart" options={options} data={data} />
-  )
+  return <Scatter id="chart" options={options} data={data} />;
 }
