8c35be5bab92e8bda8ec5086c0783fff8c581125
Juan Pinol <JuannnCodes@gmail.com>
2023-08-30 23:43:47 -0400

    Implemented a 3 tiered x axis label for day month and year.

--- src/components/Graph.jsx
+++ src/components/Graph.jsx
@@ -4,70 +4,127 @@ import {
   LinearScale,
   TimeScale,
   LogarithmicScale,
+  CategoryScale,
   PointElement,
   LineElement,
   Tooltip,
   Legend,
 } from "chart.js";
-import { Scatter } from "react-chartjs-2";
+import { Scatter, Line } from "react-chartjs-2";
 import { enUS } from "date-fns/locale";
 import format from "date-fns/format";
-import 'chartjs-adapter-date-fns';
+import "chartjs-adapter-date-fns";
 
 export default function Graph() {
-  const exampleDate = Date.now();
-  console.log(exampleDate);
+  ChartJS.register(
+    LinearScale,
+    TimeScale,
+    CategoryScale,
+    PointElement,
+    LineElement,
+    Tooltip,
+    Legend
+  );
 
-  ChartJS.register(LinearScale, TimeScale, LogarithmicScale, PointElement, LineElement, Tooltip, Legend);
+  let timestamps = [
+    1694428361748, 1695428461748, 1696428366748, 1697528361748, 1699425361748, 1693449895264, 1693549891748,
+  ]
+
+  let days = timestamps.map((date) => format(date, "d"))
+
+  let months = timestamps.map((date) => format(date, "MMMM"));
+
+  let years = timestamps.map((date) => format(date, "yyyy"));
+
+  let exampleBPData = {
+    systolic: [120, 139, 100, 110, 170, 150, 160, 190],
+    diastolic: [80, 75, 69, 85, 72, 100, 65, 61],
+  };
+
+  const data = {
+    labels: months.map((month, index) => days[index] + month + ";" + years[index]),
+    datasets: [
+      {
+        label: "Systolic",
+        data: exampleBPData.systolic,
+        backgroundColor: "rgba(255, 99, 132, 1)",
+      },
+      {
+        label: "Diastolic",
+        data: exampleBPData.diastolic,
+        backgroundColor: "rgba(155, 89, 152, 2)",
+      },
+    ],
+  };
+
+  let usedLabels = [];
 
   const options = {
-    showLine: false,
+    showLine: true,
     scales: {
       y: {
         min: 60,
         max: 180,
       },
-      x:
-        {
-          ticks: {
-            callback: function (value, index, ticks) {
-              return format(value, "d MMM");
-            },
-          },
-          type: 'time',
-          adapters: {
-            date: {
-              locale: enUS,
-            }
+      x: {
+        ticks: {
+          autoSkip: false,
+          callback: function(index) {
+            let labelOfTick = this.getLabelForValue(index)
+            console.log(labelOfTick);
+            return (days[index])
           }
         },
-        months: {
-          type: 'linear',
-          position: 'bottom',
+        type: 'category',
+        gridLines: {
+          display: false,
+          drawnOnChartArea: false,
         },
-        years: {
-          type: 'linear',
-          position: 'bottom',
+      },
+      months: {
+        type: 'category',
+        position: 'bottom',
+        gridLines: {
+          display: false,
+          drawnOnChartArea: false,
+        },
+        ticks: {
+          display: true,
+          autoSkip: false,
+          callback: function(index) {
+            // console.log(usedLabels.includes(months[index]))
+            if (!usedLabels.includes(months[index])) {
+              // console.log(months[index])
+              usedLabels.push(months[index])
+              // console.log(usedLabels);
+              return months[index]
+            } else {
+              return " "
+            }
+             }
+        },
+      },
+      years: {
+        type: 'category',
+        position: 'bottom',
+        gridLines: {
+          drawnOnChartArea: false,
+        },
+        ticks: {
+          callback: function(index) {
+            if (!usedLabels.includes(years[index])) {
+              // console.log(months[index])
+              usedLabels.push(years[index])
+              // console.log(usedLabels);
+              return years[index]
+            } else {
+              return " "
+            }
+          }
         }
+      }
     },
   };
 
-  const data = {
-    datasets: [
-      {
-        label: "Grandpa's Blood Pressure",
-        data: [
-          { x: Date.now(), y: 120 },
-          { x: 1694428361748, y: 130 },
-          { x: 1696428366748, y: 110 },
-          { x: 1691428461748, y: 100 },
-          { x: 1699425361748, y: 150 },
-          { x: 1697528361748, y: 170 },
-        ],
-        backgroundColor: "rgba(255, 99, 132, 1)",
-      },
-    ],
-  };
-
-  return <Scatter id="chart" options={options} data={data} />;
+  return <Line id="chart" options={options} data={data} />;
 }
