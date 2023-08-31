1bcc8c74b0993060a1da7acf54bdc0c9180b1ea3
Juan Pinol <JuannnCodes@gmail.com>
2023-08-31 14:12:50 -0400

    Finally settled on a date format for x axis and made it work.

--- src/components/Graph.jsx
+++ src/components/Graph.jsx
@@ -3,8 +3,6 @@ import {
   Chart as ChartJS,
   LinearScale,
   TimeScale,
-  LogarithmicScale,
-  CategoryScale,
   PointElement,
   LineElement,
   Tooltip,
@@ -19,7 +17,6 @@ export default function Graph() {
   ChartJS.register(
     LinearScale,
     TimeScale,
-    CategoryScale,
     PointElement,
     LineElement,
     Tooltip,
@@ -27,104 +24,66 @@ export default function Graph() {
   );
 
   let timestamps = [
-    1694428361748, 1695428461748, 1696428366748, 1697528361748, 1699425361748, 1693449895264, 1693549891748,
-  ]
-
-  let days = timestamps.map((date) => format(date, "d"))
-
-  let months = timestamps.map((date) => format(date, "MMMM"));
-
-  let years = timestamps.map((date) => format(date, "yyyy"));
+    1694428361748, 1694428361748, 1695428461748, 1696428366748, 1697528361748,
+    1699425361748, 1693449895264, 1693549891748,
+  ].sort();
 
   let exampleBPData = {
-    systolic: [120, 139, 100, 110, 170, 150, 160, 190],
-    diastolic: [80, 75, 69, 85, 72, 100, 65, 61],
+    systolic: [120, 140, 139, 100, 110, 170, 150, 160],
+    diastolic: [80, 100, 75, 69, 85, 72, 100, 65, 61],
   };
 
   const data = {
-    labels: months.map((month, index) => days[index] + month + ";" + years[index]),
     datasets: [
       {
         label: "Systolic",
-        data: exampleBPData.systolic,
+        data: exampleBPData.systolic.map((BP, index) => {
+          let obj = {
+            x: timestamps[index],
+            y: BP,
+          };
+          return obj;
+        }),
         backgroundColor: "rgba(255, 99, 132, 1)",
       },
       {
         label: "Diastolic",
-        data: exampleBPData.diastolic,
+        data: exampleBPData.diastolic.map((BP, index) => {
+          let obj = {
+            x: timestamps[index],
+            y: BP,
+          };
+          return obj;
+        }),
         backgroundColor: "rgba(155, 89, 152, 2)",
       },
     ],
   };
 
-  let usedLabels = [];
-
   const options = {
     showLine: true,
+    scaleShowValues: true,
     scales: {
       y: {
-        min: 60,
-        max: 180,
+        min: 50,
+        max: 190,
       },
       x: {
-        ticks: {
-          autoSkip: false,
-          callback: function(index) {
-            let labelOfTick = this.getLabelForValue(index)
-            console.log(labelOfTick);
-            return (days[index])
-          }
+        type: 'time',
+        time: {
+          parser: 'YYYY-MM-DD',
+          unit: 'month',
+          displayFormats: {
+             month: 'MM/yy'
+          },
+          tooltipFormat: 'dd/MM/yy'
         },
-        type: 'category',
-        gridLines: {
+        grid: {
           display: false,
-          drawnOnChartArea: false,
         },
       },
-      months: {
-        type: 'category',
-        position: 'bottom',
-        gridLines: {
-          display: false,
-          drawnOnChartArea: false,
-        },
-        ticks: {
-          display: true,
-          autoSkip: false,
-          callback: function(index) {
-            // console.log(usedLabels.includes(months[index]))
-            if (!usedLabels.includes(months[index])) {
-              // console.log(months[index])
-              usedLabels.push(months[index])
-              // console.log(usedLabels);
-              return months[index]
-            } else {
-              return " "
-            }
-             }
-        },
-      },
-      years: {
-        type: 'category',
-        position: 'bottom',
-        gridLines: {
-          drawnOnChartArea: false,
-        },
-        ticks: {
-          callback: function(index) {
-            if (!usedLabels.includes(years[index])) {
-              // console.log(months[index])
-              usedLabels.push(years[index])
-              // console.log(usedLabels);
-              return years[index]
-            } else {
-              return " "
-            }
-          }
-        }
-      }
     },
   };
 
-  return <Line id="chart" options={options} data={data} />;
+  return <Scatter id="chart" options={options} data={data} />;
 }
